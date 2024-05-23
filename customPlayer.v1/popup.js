605d25f1abf7352de7c3d35559cdf5cc03b82f48
Tarold <ltdecorer@gmail.com>
2024-05-24 00:43:53 +0300

    Add working customPLayer

--- customPlayer.v1/popup.js
+++ customPlayer.v1/popup.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
       chrome.tabs.sendMessage(tabs[0].id, { action: 'pause-videos' });
     });
   });
+
+  const playVideosButton = document.getElementById('play-videos-button');
+  playVideosButton.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'play-videos' });
+    });
+  });
 });
