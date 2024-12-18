c1ffcebfd10c4506014f73e1cc5312355ed1bea8
Tarold <ltdecorer@gmail.com>
2024-12-19 01:18:05 +0200

    Added megolovaniya and doom theme arduino

---
+++ tabs/tabs.py
@@ -0,0 +1,276 @@
+"""int NOTEc2 = 65;
+int NOTEd2 = 73;
+int NOTEe2 = 82;
+int NOTEf2 = 87;
+int NOTEg2 = 98;
+int NOTEa2 = 110;
+int NOTEb2 = 123;
+
+int NOTEC3 = 131;
+int NOTED3 = 147;
+int NOTEF3 = 175;
+int NOTEG3 = 196;
+int NOTEA3 = 220;
+int NOTEc3 = 131;
+int NOTEd3 = 147;
+int NOTEe3 = 165;
+int NOTEf3 = 175;
+int NOTEg3 = 196;
+int NOTEa3 = 220;
+int NOTEb3 = 247;
+
+int NOTEC4 = 262;
+int NOTED4 = 294;
+int NOTEF4 = 349;
+int NOTEG4 = 392;
+int NOTEA4 = 440;
+int NOTEc4 = 262;
+int NOTEd4 = 294;
+int NOTEe4 = 330;
+int NOTEf4 = 349;
+int NOTEg4 = 392;
+int NOTEa4 = 440;
+int NOTEb4 = 494;
+
+int NOTEC5 = 523;
+int NOTED5 = 587;
+int NOTEF5 = 698;
+int NOTEG5 = 784;
+int NOTEA5 = 880;
+int NOTEc5 = 523;
+int NOTEd5 = 587;
+int NOTEe5 = 659;
+int NOTEf5 = 698;
+int NOTEg5 = 784;
+int NOTEa5 = 880;
+int NOTEb5 = 988;"""
+"""{NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEb3, NOTEb3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEA3, NOTEA3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEb3, NOTEb3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEA3, NOTEA3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEb3, NOTEb3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEA3, NOTEA3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEb3, NOTEb3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEA3, NOTEA3, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEf4, 0, NOTEf4, NOTEf4, 0, NOTEf4, 0, NOTEf4, 0, NOTEd4, 0, 
+NOTEd4, 0, 0, NOTEd4, 0, NOTEf4, NOTEf4, NOTEf4, NOTEf4, 0, NOTEg4, 0, NOTEG4, 0, NOTEg4, NOTEf4, NOTEd4, NOTEf4, NOTEg4, 0, 0, NOTEf4, 0, NOTEf4, NOTEf4, 0, NOTEg4, 0, NOTEG4, 0, NOTEa4, 0, NOTEc5, 0, NOTEa4, 0, 0, NOTEd5, 0, NOTEd5, 0, NOTEd5, NOTEa4, NOTEd5, NOTEc5, 0, 0, 0, 0, 0, 0, 0, 0, NOTEa4, 0, NOTEa4, NOTEa4, 0, NOTEa4, 0, NOTEa4, 0, NOTEg4, 0, NOTEg4, 0, 0, 0, 0, NOTEa4, 0, NOTEa4, NOTEa4, 0, NOTEa4, 0, NOTEg4, 0, NOTEa4, 0, NOTEd5, 0, NOTEa4, NOTEg4, 0, NOTEd5, 0, NOTEa4, 0, NOTEg4, 0, NOTEf4, 0, NOTEc5, 0, NOTEa4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, 0, NOTEe4, NOTEf4, 0, NOTEa4, 0, NOTEc5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf4, NOTEd4, NOTEf4, NOTEg4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEf4, NOTEg4, 0, 0, 0, 0, 0, 0, 0, 0, NOTEG4, 0, NOTEa4, NOTEc5, 0, NOTEa4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEe4, NOTEf4, 0, NOTEg4, 0, NOTEG4, 0, NOTEc5, 0, 0, NOTEC5, 0, NOTEG4, 0, NOTEG4, NOTEg4, NOTEf4, NOTEg4, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf3, 0, NOTEg3, 0, NOTEa3, 0, NOTEf4, 0, NOTEe4, 0, 0, 0, NOTEd4, 0, 0, 0, NOTEe4, 0, 0, 0, NOTEf4, 0, 0, 0, NOTEg4, 0, 0, 0, NOTEe4, 0, 0, 0, NOTEa4, 0, 0, 0, 0, 0, 0, 0, NOTEa4, NOTEG4, NOTEg4, NOTEF4, NOTEf4, NOTEe4, NOTED4, NOTEd4, NOTEC4, 0, 0, 0, 0, 0, 0, 0, NOTED4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf4, NOTEd4, NOTEf4, NOTEg4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEe4, NOTEg4, 0, 0, 0, 0, 0, 0, 0, 0, NOTEG4, 0, NOTEa4, 0, NOTEc5, 0, NOTEa4, NOTEG4, NOTEg4, NOTEf4, NOTEd4, NOTEe4, NOTEf4, 0, NOTEg4, 0, NOTEa4, 0, NOTEc5, 0, NOTEC5, 0, NOTEG4, 0, NOTEG4, NOTEg4, NOTEf4, NOTEg4, 0, 0, 0, 0, 0, 0, NOTEf3, 0, NOTEg3, 0, NOTEa3, 0, NOTEf4, 0, NOTEe4, 0, 0, 0, NOTEd4, 0, 0, 0, NOTEe4, 0, 0, 0, NOTEf4, 0, 0, 0, NOTEg4, 0, 0, 0, NOTEe4, 0, 0, 0, NOTEa4, 0, 0, 0, 0, 0, 0, 0, NOTEa4, NOTEG4, NOTEg4, NOTEF4, NOTEf4, NOTEe4, NOTED4, NOTEd4, NOTEC4, 0, 0, 0, 0, 0, 0, 0, NOTED4, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEb3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf4, 0, 0, 0, NOTEe4, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, NOTEf4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEb3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf4, 0, 0, 0, NOTEe4, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEb3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEf4, 0, 0, 0, NOTEe4, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, NOTEf4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEb3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, NOTEf4, 0, 0, 0, NOTEe4, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, NOTEd4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEC4, NOTEC4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEd4, NOTEd4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEC4, NOTEC4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4, NOTEc4, NOTEc4, NOTEd5, 0, NOTEa4, 0, 0, NOTEG4, 0, NOTEg4, 0, NOTEf4, 0, NOTEd4, NOTEf4, NOTEg4}"""
+
+
+megolovaniya = """5|--d---------------d-------|
+4|dd--a--G-g-f-dfgcc--a--G-g|
+
+5|--------d---------------d-|
+4|-f-dfg----a--G-g-f-dfg----|
+3|------bb--------------AA--|
+
+5|--------------d-----------|
+4|a--G-g-f-dfgdd--a--G-g-f-d|
+
+5|----d---------------d-----|
+4|fgcc--a--G-g-f-dfg----a--G|
+3|------------------bb------|
+
+5|----------d---------------|
+4|-g-f-dfg----a--G-g-f-dfgdd|
+3|--------AA----------------|
+
+5|d---------------d---------|
+4|--a--G-g-f-dfgcc--a--G-g-f|
+
+5|------d---------------d---|
+4|-dfg----a--G-g-f-dfg----a-|
+3|----bb--------------AA----|
+
+5|------------d-------------|
+4|-G-g-f-dfgdd--a--G-g-f-dfg|
+
+5|--d---------------d-------|
+4|cc--a--G-g-f-dfg----a--G-g|
+3|----------------bb--------|
+
+5|--------d-----------------|
+4|-f-dfg----a--G-g-f-dfgf-ff|
+3|------AA------------------|
+
+4|-f-f-d-d--d-ffff-g-G-gfdfg|
+
+5|-------------c----d-d-d-dc|
+4|--f-ff-g-G-a---a-------a--|
+
+4|--------a-aa-a-a-g-g----a-|
+
+5|---------d----d-------c---|
+4|aa-a-g-a---ag---a-g-f---a-|
+
+5|-----------c--------------|
+4|g-f-d-ef-a----------------|
+
+4|--fdfgGgfdGgfdfg--------G-|
+
+5|-c-------------c--C-------|
+4|a--aGgfdef-g-G------G-Ggfg|
+
+4|--------------f-e---d---e-|
+3|--------f-g-a-------------|
+
+4|--f---g---e---a-------aGgF|
+
+4|feDdC-------D-------------|
+
+4|--fdfgGgfdGgfdeg--------G-|
+
+5|--c-------------c-C-------|
+4|a---aGgfdef-g-a-----G-Ggfg|
+
+4|------------f-e---d---e---|
+3|------f-g-a---------------|
+
+4|f---g---e---a-------aGgFfe|
+
+4|DdC-------D---------------|
+3|--------------------b-----|
+
+4|------f---e-------d-------|
+
+4|f-------------------------|
+
+4|------------------f---e---|
+3|------b-------------------|
+
+4|----d-----------d---------|
+
+3|----------------------b---|
+
+4|--------f---e-------d-----|
+
+4|--f-----------------------|
+
+4|--------------------f---e-|
+3|--------b-----------------|
+
+4|------d-------d-----------|
+
+5|----d---------------d-----|
+4|--dd--a--G-g-f-dfgdd--a--G|
+
+5|----------d---------------|
+4|-g-f-dfgCC--a--G-g-f-dfgcc|
+
+5|d---------------d---------|
+4|--a--G-g-f-dfgdd--a--G-g-f|
+
+5|------d---------------d---|
+4|-dfgdd--a--G-g-f-dfgCC--a-|
+
+5|------------d-------------|
+4|-G-g-f-dfgcc--a--G-g-f-dfg|"""
+doom = """3|----e-----d-----c---------|
+2|e-e---e-e---e-e---e-e-A-e-|
+
+3|----c-----e-----d-----c---|
+2|e-b---e-e---e-e---e-e---e-|
+
+3|----------------e-----d---|
+2|e-A---------e-e---e-e---e-|
+
+3|--c-------------c-----e---|
+2|e---e-e-A-e-e-b---e-e---e-|
+
+3|--d-----c-----------------|
+2|e---e-e---e-e-A---------e-|
+
+3|--e-----d-----c-----------|
+2|e---e-e---e-e---e-e-A-e-e-|
+
+3|--c-----e-----d-----c-----|
+2|b---e-e---e-e---e-e---e-e-|
+
+3|--------------e-----d-----|
+2|A---------e-e---e-e---e-e-|
+
+3|c-------------c-----e-----|
+2|--e-e-A-e-e-b---e-e---e-e-|
+
+3|d-----Fd--aF--dF-aFd------|
+2|--e-e----b---b-------be-e-|
+
+3|e-----d-----c-------------|
+2|--e-e---e-e---e-e-A-e-e-b-|
+
+3|c-----e-----d-----c-------|
+2|--e-e---e-e---e-e---e-e-A-|
+
+3|------------e-----d-----c-|
+2|--------e-e---e-e---e-e---|
+
+3|------------c-----e-----d-|
+2|e-e-A-e-e-b---e-e---e-e---|
+
+4|-----------e---e-g-b------|
+3|----bg-egb--gb--b-------a-|
+2|e-e-----------------a-a---|
+
+3|----g-----f-----D-----e-f-|
+2|a-a---a-a---a-a---a-a-----|
+
+3|----a-----g-----f-----D---|
+2|a-a---a-a---a-a---a-a-----|
+
+3|----------a-----g-----f---|
+2|------a-a---a-a---a-a---a-|"""
+lines = megolovaniya.split("""
+
+""")
+
+split_lines = [line.split() for line in lines]
+
+
+def convert_string_to_array(input_string):
+    ar = input_string.split('|')
+    octave = int(ar[0]) + 1
+    tabs = ar[1]
+    result = []
+    for tab in tabs:
+        if tab != '-':
+            result.append('NOTE'+tab + str(octave))
+        else:
+            result.append(tab)
+    return result
+
+
+def megre_in_on(arrays):
+    result = []
+    for i in range(len(arrays[0])):
+        tab = ''
+        for j in range(len(arrays)):
+            tab += arrays[j][i] if arrays[j][i] != '-' else ''
+        result.append(tab)
+    return result
+
+
+transformed_lines = [[convert_string_to_array(
+    line) for line in split_line] for split_line in split_lines]
+merged_lines = [megre_in_on(split_line) for split_line in transformed_lines]
+
+
+def flatten_and_unique(nested_lists):
+    # Создаем множество для хранения уникальных строк
+    unique_strings = set()
+
+    # Проходим по каждому подсписку и добавляем строки во множество
+    for sublist in nested_lists:
+        for item in sublist:
+            unique_strings.add(item)
+
+    # Преобразуем множество обратно в список и сортируем его
+    sorted_unique_strings = sorted(unique_strings)
+
+    return sorted_unique_strings
+
+
+def flatten_extend(matrix):
+    flat_list = []
+    for row in matrix:
+        flat_list.extend(row)
+    return flat_list
+
+
+def create_formatted_string(note_list):
+    formatted_list = ["0" if item == '' else item for item in note_list]
+    return "{" + ", ".join(formatted_list) + "}"
+
+
+print(flatten_and_unique(merged_lines))
